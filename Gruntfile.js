091675325547d7161147119e13c86b2980f63160
Marshall Farrier <info@marshallfarrier.com>
2015-04-05 23:50:00 -0700

    MongoDB installation steps.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,19 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        exec: {
-            echo_hello: 'echo "hello"'
-        }
+    /**
+     * shelljs : http://documentup.com/arturadib/shelljs
+     * mongoDB : http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+     */
+    grunt.registerTask('default', function() {
+        var shell = require('shelljs'),
+            cmds = [
+                'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10',
+                'echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list',
+                'apt-get update',
+                'apt-get install -y mongodb-org=3.0.1'
+            ];
+        
+        cmds.forEach(function(cmd, i, arr) {
+            shell.exec(cmd);
+        });
     });
-
-    grunt.loadNpmTasks('grunt-exec');
-
-    grunt.registerTask('default', ['exec']);
 };
